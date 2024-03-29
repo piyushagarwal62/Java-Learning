24080e5e89e155ff50ae789cb6d6f4865847c974
Piyush Agarwal <piyush.agarwal@questionpro.com>
2024-03-29 23:40:46 +0530

    Adding git ignore file and removing Java Basic directory for time being.

--- JavaBasics/src/TempClass.java
+++
@@ -1,5 +0,0 @@
-public class TempClass {
-    public static void main(String[] args) {
-        System.out.println("Print");
-    }
-}
---
+++ JavaVersions/src/main/java/com/codebypiyush/java7/Main.java
@@ -0,0 +1,7 @@
+package com.codebypiyush.java7;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
